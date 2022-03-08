7ef992b01cbbb03a6b3a5ee847d8a559043d6a59
AlexVerkhov <alexverkhov@gmail.com>
2022-03-08 20:27:39 +0700

    second task done

--- Lesson 2.3.1 - login form with different views/MainViewController.swift
+++ Lesson 2.3.1 - login form with different views/MainViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet var inputUserName: UITextField!
     @IBOutlet var inputPassword: UITextField!
@@ -15,9 +15,23 @@ class MainViewController: UIViewController {
     private let userName = "admin"
     private let password = "password"
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     
+        inputUserName.delegate = self
+        inputUserName.returnKeyType = .next
+   
+        inputPassword.delegate = self
+        inputPassword.returnKeyType = .done
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let accountViewController = segue.destination as? AccountViewController else { return }
         accountViewController.userName = inputUserName.text
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
     }
     
     @IBAction func unwind(for segue: UIStoryboardSegue) {
@@ -26,6 +40,13 @@ class MainViewController: UIViewController {
     
     @IBAction func logInButtonClicked() {
         if !canLogin() {
+            
+            clearInputs()
+            
+            // заметил, что если перенести эту строку за вызов алерта, фокус не перейдет
+            // на инпут для логина
+            inputUserName.becomeFirstResponder()
+            
             alertWithAction(
                 title: "Incorrect",
                 description: "Incorrect Username or Password",
@@ -35,7 +56,13 @@ class MainViewController: UIViewController {
             return
         }
         
-        //performSegue(withIdentifier: "accountSegue", sender: nil)
+        // похоже, что алерт прерывает сигвей, но меня не покидает чувство, что сделал-таки неверно
+        // так как если закомментировать алерт, сигвей все равно пройдет
+        // но если я пробую дернуть сигвей вручную - он все равно должен быть связан с какой-то
+        // кнопкой... отвязать его от текущей кнопки, сделать невидимую или невидимый элемент навигации?
+        // и далее дергать таким образом, как в примере ниже?
+        //
+        // performSegue(withIdentifier: "accountSegue", sender: nil)
     }
     
     @IBAction func forgotUserNameButtonClicked() {
@@ -52,6 +79,15 @@ class MainViewController: UIViewController {
             description: "Password is \"\(password)\"",
             buttonTitle: "Ok"
         )
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == inputPassword {
+            logInButtonClicked()
+        } else if textField == inputUserName {
+            inputPassword.becomeFirstResponder()
+        }
+        return true
     }
     
     private func canLogin() -> Bool {
